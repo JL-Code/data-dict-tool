4e32713d358f00f43647f7372bbdbe1426a4ff6d
蒋勇 <jiangy@highzap.com>
2021-08-21 13:07:13 +0800

    update:table-metadata-sql

--- execl.py
+++ execl.py
@@ -5,28 +5,25 @@ import xlwings as xw
 if __name__ == '__main__':
 
     # 使用 json 库读取 json 文件
-    with open("./data/data2.json") as json_file:
+    with open("./data/map.json") as json_file:
         data = json.load(json_file)
 
-        # 获取 xlwings 应用实例
+    # 获取 xlwings 应用实例
     app = xw.App(visible=True, add_book=False)
 
     # 添加一个工作簿
     wb = app.books.add()
-
+    prev = ''
     for key in data:
         df = pd.DataFrame(data[key])
-        curr_sheet = wb.sheets.add(key)
-        curr_sheet.Range('A1').value = df
+        if prev == '':
+            curr_sheet = wb.sheets.add(key)
+        else:
+            curr_sheet = wb.sheets.add(key, after=prev)
+        curr_sheet.range('A2').value = df
+        prev = key
 
     # 保存 excel
-    wb.save("test1.xlsx")
-    # 退出工作簿
-    wb.close()
-    # 退出应用
-    app.quit()
+    wb.save("/Users/codeme/Library/Group Containers/UBF8T346G9.Office/数据字典.xlsx")
 
-    # print(df1)
-    # print(df2)
-    # print(df3)
-    # print(df4)
+    app.quit()
