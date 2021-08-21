bd16a6db2844a5a1d233aaf622e38184a269d5fb
蒋勇 <jiangy@highzap.com>
2021-08-21 14:44:13 +0800

    update execl case

--- execl.py
+++ execl.py
@@ -5,17 +5,19 @@ import xlwings as xw
 if __name__ == '__main__':
 
     # 使用 json 库读取 json 文件
-    with open("./data/map.json") as json_file:
+    with open("./data/map.json", 'rb') as json_file:
         data = json.load(json_file)
 
     # 获取 xlwings 应用实例
     app = xw.App(visible=True, add_book=False)
-
+    # 下面代码实现了将df中的column列作为index
+    # df.set_index(["Column"], inplace=True)
     # 添加一个工作簿
     wb = app.books.add()
     prev = ''
     for key in data:
         df = pd.DataFrame(data[key])
+        df.set_index(["序号"], inplace=True)
         if prev == '':
             curr_sheet = wb.sheets.add(key)
         else:
@@ -24,6 +26,6 @@ if __name__ == '__main__':
         prev = key
 
     # 保存 excel
-    wb.save("/Users/codeme/Library/Group Containers/UBF8T346G9.Office/数据字典.xlsx")
+    # wb.save("D:\\Workspace\\python-execl\\数据字典.xlsx")
 
-    app.quit()
+    # app.quit()
