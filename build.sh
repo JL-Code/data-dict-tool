39ae4150d437ec888d60179207ae54c144db42e0
蒋勇 <jiangy@highzap.com>
2021-12-25 23:48:31 +0800

    sytle: reformat file

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
 # 单文件打包：
 pyinstaller -F -w --add-data "./qt/view:./view" --add-data "./qt/execl_util.py:./" --add-data "./qt/sql_util.py:./" ./qt/main.py
-# 单文件夹打包：
-#pyinstaller -D -w --add-data "./qt/view:./view" --add-data "./qt/execl_util.py:./" --add-data "./qt/sql_util.py:./" ./qt/main.py
