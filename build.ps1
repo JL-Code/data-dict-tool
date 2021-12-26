663462282f4e125aacad92e383445c467f2036de
蒋勇 <jiangy@highzap.com>
2021-12-26 13:54:22 +0800

    build: 更新构建脚本,修复路径引导错误的问题

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 # 单文件打包：
-pyinstaller -F -w --add-data "./view:./view" --add-data "./execl_util.py:./" --add-data "./sql_util.py:./" ./main.py
+pyinstaller -F -w --add-data "./qt/view:./view" --add-data "./qt/execl_util.py:./" --add-data "./qt/sql_util.py:./" ./main.py
 # 单文件夹打包：
-# pyinstaller -D -w --add-data "./view:./view" --add-data "./execl_util.py:./" --add-data "./sql_util.py:./" ./main.py
+# pyinstaller -D -w --add-data "./qt/view:./view" --add-data "./qt/execl_util.py:./" --add-data "./qt/sql_util.py:./" ./main.py
