fa0455e973b91aa5a75da68261596cf70855d634
蒋勇 <jiangy@highzap.com>
2021-12-26 13:49:40 +0800

    build: 更新构建脚本

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 # 单文件打包：
 pyinstaller -F -w --add-data "./view:./view" --add-data "./execl_util.py:./" --add-data "./sql_util.py:./" ./main.py
+# 单文件夹打包：
+# pyinstaller -D -w --add-data "./view:./view" --add-data "./execl_util.py:./" --add-data "./sql_util.py:./" ./main.py
