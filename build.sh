81ec446fdb6a1ff5c2770c878b29bb511e3a4069
蒋勇 <jiangy@highzap.com>
2022-02-25 12:25:31 +0800

    style: 更新build.sh TODO

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
+# TODO: 支持参数选择不同打包方式
 # 单文件打包：
-#pyinstaller -F -w -i ./logo.ico --add-data "./qt/view:./qt/view" --add-data "./qt/execl_util_openpyxl.py:./qt" --add-data "./qt/sql_util.py:./qt" ./data_dict.py
+pyinstaller -F -w -i ./logo.ico --add-data "./qt/view:./qt/view" --add-data "./qt/execl_util_openpyxl.py:./qt" --add-data "./qt/sql_util.py:./qt" ./data_dict.py
 # 单文件夹打包：
-pyinstaller -D -w -i ./logo.ico --add-data "./qt/view:./qt/view" --add-data "./qt/execl_util.py:./qt" --add-data "./qt/sql_util.py:./qt" ./data_dict.py
+#pyinstaller -D -w -i ./logo.ico --add-data "./qt/view:./qt/view" --add-data "./qt/execl_util.py:./qt" --add-data "./qt/sql_util.py:./qt" ./data_dict.py
